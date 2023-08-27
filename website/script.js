8568273403ef404c027bdb08907bf99e8fa54beb
eirikio <88273431+eirikio@users.noreply.github.com>
2023-08-28 01:15:55 +0200

    images and links not working.. updating

--- website/script.js
+++ website/script.js
@@ -1,6 +1,6 @@
 document.querySelector("button").addEventListener("click", function() {
     const link = document.createElement("a")
     link.setAttribute("download", "BabyBotv2.rar")
-    link.setAttribute("href", "/website/BabyBotV2.rar")
+    link.setAttribute("href", "\website\BabyBotV2.rar")
     link.click()
 })
