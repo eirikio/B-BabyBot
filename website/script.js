007a61c7102e9016acc124875d08b5d48a51e849
eirikio <88273431+eirikio@users.noreply.github.com>
2023-08-28 00:54:05 +0200

    babybot v2.exe has been added to the download button of the website

--- website/script.js
+++ website/script.js
@@ -1,6 +1,6 @@
 document.querySelector("button").addEventListener("click", function() {
     const link = document.createElement("a")
-    link.setAttribute("download", "dl-icon.png")
-    link.setAttribute("href", "/dl-icon.png")
+    link.setAttribute("download", "BabyBotv2.rar")
+    link.setAttribute("href", "/B-BabyBot v2/BabyBotv2.rar")
     link.click()
 })
