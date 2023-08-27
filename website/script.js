acbc93aca7f7f67deb82efdd6260d2364ed7d357
eirikio <88273431+eirikio@users.noreply.github.com>
2023-08-28 00:59:21 +0200

    updating download button

--- website/script.js
+++ website/script.js
@@ -1,6 +1,6 @@
 document.querySelector("button").addEventListener("click", function() {
     const link = document.createElement("a")
     link.setAttribute("download", "BabyBotv2.rar")
-    link.setAttribute("href", "/B-BabyBot v2/BabyBotv2.rar")
+    link.setAttribute("href", "D:/Projects/B-BabyBot/B-BabyBot v2/BabyBotv2")
     link.click()
 })
