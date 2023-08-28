37fa98422d1da6557bc5464aadc6346dd0d26b72
eirikio <88273431+eirikio@users.noreply.github.com>
2023-08-28 12:15:59 +0200

    dl link

--- website/script.js
+++ website/script.js
@@ -1,6 +1,6 @@
 document.querySelector("button").addEventListener("click", function() {
     const link = document.createElement("a")
     link.setAttribute("download", "BabyBotV2.rar")
-    link.setAttribute("href", "/website/BabyBotV2.rar")
+    link.setAttribute("href", "static/BabyBotV2.rar")
     link.click()
 })
